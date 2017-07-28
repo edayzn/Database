bb6ffcd6a2f0f77d2d64a7abfb8a7fbd7e2616b0
EDA YZN <eda.yzn5@gmail.com>
2017-07-28 11:28:20 +0300

    initial commit

--- src/Utility.java
+++ src/Utility.java
@@ -8,41 +8,43 @@ import java.sql.ResultSet;
 
 
 public class Utility {
-    public static String sgl;
+ // public static String sgl;
+    Connection connection = null;
 
-    public void Goster() throws SQLException {
-        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+    public static void main(String[] args) throws SQLException {
+        Utility gst = new Utility();
+        gst.goster();
+    }
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Where is your MySQL JDBC Driver?");
+    public  void baglan(){
+       System.out.println("-------- MySQL JDBC Connection Testing ------------");
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+        System.out.println("Where is your MySQL JDBC Driver?");
+        Logger.getAnonymousLogger("Hayda!" + e);
+        return;
+    }
+    System.out.println("MySQL JDBC Driver Registered!");
+    try {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/proje", "root", "123456");
+    } catch (SQLException e) {
+        System.out.println("Connection Failed! Check output console");
+        Logger.getAnonymousLogger("Hayda!" + e);
+        return;
+    }
+    if (connection != null) {
+        System.out.println("You made it, take control your database now!");
+    } else {
+        System.out.println("Failed to make connection!");
+    }
+    }
 
-            Logger.getAnonymousLogger("Ops!" + e);
-            return;
-        }
-
-        System.out.println("MySQL JDBC Driver Registered!");
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/proje", "root", "123456");
-
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-
-            Logger.getAnonymousLogger("Ops!" + e);
-            return;
-        }
-
-        if (connection != null) {
-            System.out.println("You made it, take control your database now!");
-        } else {
-            System.out.println("Failed to make connection!");
-        }
-        sgl = "SELECT pkid,adi,ozellik,fiyat,altkategoriId From urunler";
+    public void goster() throws SQLException {
+        baglan();
+        StringBuffer sgl = new StringBuffer("SELECT pkid,adi,ozellik,fiyat,altkategoriId From urunler");
         Statement stm = connection.createStatement();
-        ResultSet rs = stm.executeQuery(sgl);
+        ResultSet rs = stm.executeQuery(String.valueOf(sgl));
         while (rs.next()) {
 
             int pkid = rs.getInt("pkid");
@@ -60,11 +62,6 @@ public class Utility {
         }
         connection.close();
         rs.close();
-    }
-
-    public static void main(String[] args) throws SQLException {
-        Utility gst = new Utility();
-        gst.Goster();
     }
 }
 
